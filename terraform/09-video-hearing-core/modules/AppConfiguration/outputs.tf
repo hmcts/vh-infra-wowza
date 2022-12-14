fde6a5a890174fe54cf95fb96267035aa8769f8b
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-12-14 17:34:32 +0000

    initial remove of core

--- terraform/09-video-hearing-core/modules/AppConfiguration/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "connection_string" {
-  value = azurerm_app_configuration.vh.primary_write_key[0].connection_string
-}
--- terraform/09-video-hearing-core/modules/Monitoring/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.vh-infra-core.instrumentation_key
-}
