136162833dd6c0c77ea4d05871d7fdb6cb428ca1
Your Name <james.ferrari@version1.com>
2022-12-29 09:42:18 +0000

    Change NSG Rules from Nested

--- terraform/10-video-hearing-wowza/modules/wowza/network.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/network.tf
@@ -46,82 +46,95 @@ resource "azurerm_network_security_group" "wowza" {
   resource_group_name = azurerm_resource_group.wowza.name
   location            = azurerm_resource_group.wowza.location
 
-  security_rule {
-    name                       = "DenyAllAzureLoadBalancerInbound"
-    priority                   = 4096
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_address_prefix      = "AzureLoadBalancer"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "DenyAllVnetInbound"
-    priority                   = 4095
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "AllowWowzaSSH"
-    priority                   = 1010
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["22"]
-    source_address_prefixes    = [var.address_space]
-    destination_address_prefix = var.address_space
-  }
-
-  security_rule {
-    name                       = "AllowPexip"
-    priority                   = 1020
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["443", "8087"]
-    source_address_prefixes    = ["35.242.170.203", "35.246.8.138", "34.89.88.216", "35.242.163.246", "34.89.25.191", "35.189.100.86", "35.234.131.131", "35.246.37.95", "34.89.37.194", "35.246.58.129"]
-    destination_address_prefix = var.address_space
-  }
-
-  security_rule {
-    name                       = "AllowAKSInbound"
-    priority                   = 1030
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["443", "8087"]
-    source_address_prefix      = lookup(local.aks_address, var.environment, "*")
-    destination_address_prefix = var.address_space
-  }
-
-  security_rule {
-    name                       = "AllowAzureLoadBalancerProbes"
-    priority                   = 1050
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "AzureLoadBalancer"
-    source_port_range          = "*"
-    destination_port_ranges    = ["22", "443", "8087"]
-    destination_address_prefix = var.address_space
-  }
-
   tags = var.tags
 
   depends_on = [
     azurerm_linux_virtual_machine.wowza
   ]
+}
+
+resource "azurerm_network_security_rule" "DenyAllAzureLoadBalancerInbound" {
+  name                        = "DenyAllAzureLoadBalancerInbound"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 4096
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "Tcp"
+  source_address_prefix       = "AzureLoadBalancer"
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "*"
+}
+
+resource "azurerm_network_security_rule" "DenyAllVnetInbound" {
+  name                        = "DenyAllVnetInbound"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 4095
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "Tcp"
+  source_address_prefix       = "VirtualNetwork"
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "*"
+}
+
+resource "azurerm_network_security_rule" "AllowWowzaSSH" {
+  name                        = "AllowWowzaSSH"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 1010
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = var.address_space
+  source_port_range           = "*"
+  destination_address_prefix  = var.address_space
+  destination_port_ranges     = ["22"]
+}
+
+resource "azurerm_network_security_rule" "AllowPexip" {
+  name                        = "AllowPexip"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 1020
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefixes     = ["35.242.170.203", "35.246.8.138", "34.89.88.216", "35.242.163.246", "34.89.25.191", "35.189.100.86", "35.234.131.131", "35.246.37.95", "34.89.37.194", "35.246.58.129"]
+  source_port_range           = "*"
+  destination_address_prefix  = var.address_space
+  destination_port_ranges     = ["443", "8087"]
+}
+
+resource "azurerm_network_security_rule" "AllowAKSInbound" {
+  name                        = "AllowAKSInbound"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 1030
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = lookup(local.aks_address, var.environment, "*")
+  source_port_range           = "*"
+  destination_address_prefix  = var.address_space
+  destination_port_ranges     = ["443", "8087"]
+}
+
+resource "azurerm_network_security_rule" "AllowAzureLoadBalancerProbes" {
+  name                        = "AllowAzureLoadBalancerProbes"
+  resource_group_name         = azurerm_resource_group.wowza.name
+  network_security_group_name = azurerm_network_security_group.wowza.name
+  priority                    = 1050
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = "AzureLoadBalancer"
+  source_port_range           = "*"
+  destination_address_prefix  = var.address_space
+  destination_port_ranges     = ["443", "8087", "22"]
 }
 
 resource "azurerm_network_security_rule" "AllowDynatrace" {
