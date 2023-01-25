5772194db7114687e7ac2d51ca0cbba8357ad758
Your Name <james.ferrari@version1.com>
2023-01-25 15:05:40 +0000

    add azurerm backend

--- terraform/infra/_providers.tf
+++ terraform/infra/_providers.tf
@@ -14,6 +14,13 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
+  alias                      = "private-endpoint-dns"
+  skip_provider_registration = "true"
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+}
+
+provider "azurerm" {
+  features {}
   alias                      = "hearings-dns"
   skip_provider_registration = "true"
   subscription_id            = "4bb049c8-33f3-4860-91b4-9ee45375cc18"
@@ -81,11 +88,3 @@ provider "azurerm" {
   client_secret   = var.network_client_secret
   tenant_id       = var.network_tenant_id
 }
-
-provider "azurerm" {
-  features {}
-  alias                      = "private-endpoint-dns"
-  skip_provider_registration = "true"
-  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
-}
-
