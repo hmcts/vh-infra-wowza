1df529be96542e6d722d424ac1cfd1bc20cab1aa
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-09-29 15:04:31 +0100

    use vars

--- terraform/infra/_providers.tf
+++ terraform/infra/_providers.tf
@@ -92,5 +92,5 @@ provider "azurerm" {
 provider "azurerm" {
   features {}
   alias           = "dcd-cnp"
-  subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9" # dcd-cnp-DEV
+  subscription_id = var.dcd_cnp_subscription # dcd-cnp-DEV
 }
