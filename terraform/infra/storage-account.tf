be557ba49e200c6fc89ede481d9bedba33da5ebb
Sam Kompfner <sam.kompfner@hmcts.net>
2024-12-04 23:25:18 +0000

    add import for container

--- terraform/infra/storage-account.tf
+++ terraform/infra/storage-account.tf
@@ -7,10 +7,6 @@ locals {
   recordings_container_name = "recordings"
 }
 
-import {
-  to = module.wowza_recordings.azurerm_storage_container.container["recordings"]
-  id = "/subscriptions/867a878b-cb68-4de5-9741-361ac9e178b6/resourceGroups/vh-infra-wowza-dev/providers/Microsoft.Storage/storageAccounts/vhinfrawowzadev/blobServices/default/containers/recordings"
-}
 
 #tfsec:ignore:azure-storage-default-action-deny
 module "wowza_recordings" {
