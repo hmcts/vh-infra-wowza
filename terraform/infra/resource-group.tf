60492724cf539a990a41afc21e84d416fd8c182d
Sam Kompfner <sam.kompfner@hmcts.net>
2025-01-30 11:33:52 +0000

    add role assignments

--- terraform/infra/resource-group.tf
+++ terraform/infra/resource-group.tf
@@ -3,3 +3,14 @@ resource "azurerm_resource_group" "wowza" {
   location = var.location
   tags     = local.common_tags
 }
+
+###############################################################
+# Wowza RG Group Access #######################################
+###############################################################
+
+resource "azurerm_role_assignment" "dts_vh_contributors_prod_access" {
+  count                = var.environment == "prod" ? 1 : 0
+  scope                = azurerm_resource_group.wowza.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azuread_group.dts_vh_contributors_prod.object_id
+}
