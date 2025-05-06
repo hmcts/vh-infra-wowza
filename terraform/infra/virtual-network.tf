6f2eb15502244b6bc7953451200a59f1460ec502
Sam Kompfner <sam.kompfner@hmcts.net>
2025-05-06 22:26:17 +0100

    add private_endpoint_network_policies

--- terraform/infra/virtual-network.tf
+++ terraform/infra/virtual-network.tf
@@ -13,4 +13,5 @@ resource "azurerm_subnet" "wowza" {
   address_prefixes     = [var.address_space]
 
   private_link_service_network_policies_enabled = true
+  private_endpoint_network_policies             = "Enabled"
 }
