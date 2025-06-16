3208416f1757c1ca7317608ff35e2c334172498d
Sam Kompfner <sam.kompfner@hmcts.net>
2025-06-16 11:30:42 +0100

    re-add wowza prod storage and rg

--- terraform/infra/managed-identity.tf
+++ terraform/infra/managed-identity.tf
@@ -88,66 +88,66 @@ resource "azurerm_role_assignment" "wowza-sa-tag-role-prod" {
   ]
 }
 
-###############################################################
-# Automation Account MI. ######################################
-###############################################################
+# ###############################################################
+# # Automation Account MI. ######################################
+# ###############################################################
 
-resource "azurerm_user_assigned_identity" "wowza-automation-account-mi" {
-  name                = "wowza-automation-mi-${var.environment}"
-  resource_group_name = azurerm_resource_group.wowza.name
-  location            = azurerm_resource_group.wowza.location
+# resource "azurerm_user_assigned_identity" "wowza-automation-account-mi" {
+#   name                = "wowza-automation-mi-${var.environment}"
+#   resource_group_name = azurerm_resource_group.wowza.name
+#   location            = azurerm_resource_group.wowza.location
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "azurerm_role_definition" "virtual-machine-control" {
-  name        = "Virtual-Machine-Control-${var.environment}"
-  scope       = azurerm_resource_group.wowza.id
-  description = "Custom Role for controlling virtual machines"
+# resource "azurerm_role_definition" "virtual-machine-control" {
+#   name        = "Virtual-Machine-Control-${var.environment}"
+#   scope       = azurerm_resource_group.wowza.id
+#   description = "Custom Role for controlling virtual machines"
 
-  permissions {
-    actions = [
-      "Microsoft.Compute/virtualMachines/read",
-      "Microsoft.Compute/virtualMachines/start/action",
-      "Microsoft.Compute/virtualMachines/deallocate/action",
-    ]
-    not_actions = []
-  }
+#   permissions {
+#     actions = [
+#       "Microsoft.Compute/virtualMachines/read",
+#       "Microsoft.Compute/virtualMachines/start/action",
+#       "Microsoft.Compute/virtualMachines/deallocate/action",
+#     ]
+#     not_actions = []
+#   }
 
-  assignable_scopes = [
-    azurerm_resource_group.wowza.id,
-  ]
-}
+#   assignable_scopes = [
+#     azurerm_resource_group.wowza.id,
+#   ]
+# }
 
-resource "azurerm_role_assignment" "wowza-auto-acct-mi-role" {
-  scope              = azurerm_resource_group.wowza.id
-  role_definition_id = azurerm_role_definition.virtual-machine-control.role_definition_resource_id
-  principal_id       = azurerm_user_assigned_identity.wowza-automation-account-mi.principal_id
+# resource "azurerm_role_assignment" "wowza-auto-acct-mi-role" {
+#   scope              = azurerm_resource_group.wowza.id
+#   role_definition_id = azurerm_role_definition.virtual-machine-control.role_definition_resource_id
+#   principal_id       = azurerm_user_assigned_identity.wowza-automation-account-mi.principal_id
 
-  depends_on = [
-    azurerm_role_definition.virtual-machine-control
-  ]
-}
+#   depends_on = [
+#     azurerm_role_definition.virtual-machine-control
+#   ]
+# }
 
-###############################################################
-# Key Vault Access. ###########################################
-###############################################################
+# ###############################################################
+# # Key Vault Access. ###########################################
+# ###############################################################
 
-data "azurerm_key_vault" "acmekv" {
-  name                = "acmedtssds${var.environment}"
-  resource_group_name = "sds-platform-${var.environment}-rg"
-}
+# data "azurerm_key_vault" "acmekv" {
+#   name                = "acmedtssds${var.environment}"
+#   resource_group_name = "sds-platform-${var.environment}-rg"
+# }
 
-resource "azurerm_user_assigned_identity" "wowza_cert" {
-  resource_group_name = azurerm_resource_group.wowza.name
-  location            = azurerm_resource_group.wowza.location
+# resource "azurerm_user_assigned_identity" "wowza_cert" {
+#   resource_group_name = azurerm_resource_group.wowza.name
+#   location            = azurerm_resource_group.wowza.location
 
-  name = "vh-wowza-cert-${var.environment}-mi"
-  tags = local.common_tags
-}
+#   name = "vh-wowza-cert-${var.environment}-mi"
+#   tags = local.common_tags
+# }
 
-resource "azurerm_role_assignment" "kv_access" {
-  scope                = data.azurerm_key_vault.acmekv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_user_assigned_identity.wowza_cert.principal_id
-}
+# resource "azurerm_role_assignment" "kv_access" {
+#   scope                = data.azurerm_key_vault.acmekv.id
+#   role_definition_name = "Key Vault Secrets User"
+#   principal_id         = azurerm_user_assigned_identity.wowza_cert.principal_id
+# }
