21564d7e72990776fef3d6bacc3de93df4c4d4ab
Sam Kompfner <sam.kompfner@hmcts.net>
2025-06-16 11:49:57 +0100

    remove data

--- terraform/infra/network-security-group.tf
+++ terraform/infra/network-security-group.tf
@@ -203,28 +203,28 @@ resource "azurerm_network_security_rule" "AllowF5VPNSSH" {
   destination_port_ranges     = ["22", "443", "80", "8087"]
 }
 
-resource "azurerm_network_watcher_flow_log" "nsg" {
-  name                 = "${var.service_name}-flow-logs"
-  network_watcher_name = "NetworkWatcher_${azurerm_resource_group.wowza.location}"
-  resource_group_name  = "NetworkWatcherRG"
-  location             = data.azurerm_log_analytics_workspace.core.location
+# resource "azurerm_network_watcher_flow_log" "nsg" {
+#   name                 = "${var.service_name}-flow-logs"
+#   network_watcher_name = "NetworkWatcher_${azurerm_resource_group.wowza.location}"
+#   resource_group_name  = "NetworkWatcherRG"
+#   location             = data.azurerm_log_analytics_workspace.core.location
 
-  target_resource_id = azurerm_network_security_group.wowza.id
-  storage_account_id = module.wowza_recordings.storageaccount_id
-  enabled            = true
+#   target_resource_id = azurerm_network_security_group.wowza.id
+#   storage_account_id = module.wowza_recordings.storageaccount_id
+#   enabled            = true
 
-  retention_policy {
-    enabled = true
-    days    = 7
-  }
+#   retention_policy {
+#     enabled = true
+#     days    = 7
+#   }
 
-  traffic_analytics {
-    enabled               = true
-    workspace_id          = data.azurerm_log_analytics_workspace.core.workspace_id
-    workspace_region      = data.azurerm_log_analytics_workspace.core.location
-    workspace_resource_id = data.azurerm_log_analytics_workspace.core.id
-    interval_in_minutes   = 10
-  }
+#   traffic_analytics {
+#     enabled               = true
+#     workspace_id          = data.azurerm_log_analytics_workspace.core.workspace_id
+#     workspace_region      = data.azurerm_log_analytics_workspace.core.location
+#     workspace_resource_id = data.azurerm_log_analytics_workspace.core.id
+#     interval_in_minutes   = 10
+#   }
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
