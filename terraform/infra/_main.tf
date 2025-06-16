140883133dc59f4609ea088aebb85339c9b9c02c
Sam Kompfner <sam.kompfner@hmcts.net>
2025-06-16 11:44:09 +0100

    remove data

--- terraform/infra/_main.tf
+++ terraform/infra/_main.tf
@@ -8,15 +8,15 @@ data "azurerm_resource_group" "vh-infra-core" {
   name = "vh-infra-core-${var.environment}"
 }
 
-data "azurerm_key_vault_secret" "dynatrace_token" {
-  name         = "dynatrace-token"
-  key_vault_id = data.azurerm_key_vault.vh-infra-core.id
-}
+# data "azurerm_key_vault_secret" "dynatrace_token" {
+#   name         = "dynatrace-token"
+#   key_vault_id = data.azurerm_key_vault.vh-infra-core.id
+# }
 
-data "azurerm_key_vault" "vh-infra-core" {
-  name                = "vh-infra-core-${var.environment}"
-  resource_group_name = data.azurerm_resource_group.vh-infra-core.name
-}
+# data "azurerm_key_vault" "vh-infra-core" {
+#   name                = "vh-infra-core-${var.environment}"
+#   resource_group_name = data.azurerm_resource_group.vh-infra-core.name
+# }
 
 # data "azurerm_key_vault_certificate" "vh-wildcard" {
 #   name         = "wildcard-hearings-reform-hmcts-net"
@@ -29,10 +29,10 @@ data "azurerm_private_dns_zone" "core-infra-intsvc" {
   resource_group_name = "core-infra-intsvc-rg"
 }
 
-data "azurerm_log_analytics_workspace" "core" {
-  name                = "vh-infra-core-${var.environment}"
-  resource_group_name = "vh-infra-core-${var.environment}"
-}
+# data "azurerm_log_analytics_workspace" "core" {
+#   name                = "vh-infra-core-${var.environment}"
+#   resource_group_name = "vh-infra-core-${var.environment}"
+# }
 
 data "azurerm_user_assigned_identity" "vh_mi" {
   name                = "vh-${var.environment}-mi"
