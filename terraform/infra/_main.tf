cfc0695021c2fdb32d02719d728b3337d36b2011
Sam Kompfner <sam.kompfner@hmcts.net>
2025-06-25 12:12:32 +0100

    remove data

--- terraform/infra/_main.tf
+++ terraform/infra/_main.tf
@@ -8,26 +8,26 @@ data "azurerm_subscription" "current" {
 #   name = "vh-infra-core-${var.environment}"
 # }
 
-data "azurerm_key_vault_secret" "dynatrace_token" {
-  name         = "dynatrace-token"
-  key_vault_id = data.azurerm_key_vault.vh-infra-core.id
-}
+# data "azurerm_key_vault_secret" "dynatrace_token" {
+#   name         = "dynatrace-token"
+#   key_vault_id = data.azurerm_key_vault.vh-infra-core.id
+# }
 
-data "azurerm_key_vault" "vh-infra-core" {
-  name                = "vh-infra-core-${var.environment}"
-  resource_group_name = data.azurerm_resource_group.vh-infra-core.name
-}
+# data "azurerm_key_vault" "vh-infra-core" {
+#   name                = "vh-infra-core-${var.environment}"
+#   resource_group_name = data.azurerm_resource_group.vh-infra-core.name
+# }
 
 # data "azurerm_key_vault_certificate" "vh-wildcard" {
 #   name         = "wildcard-hearings-reform-hmcts-net"
 #   key_vault_id = data.azurerm_key_vault.vh-infra-core.id
 # }
 
-data "azurerm_private_dns_zone" "core-infra-intsvc" {
-  provider            = azurerm.private-endpoint-dns
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "core-infra-intsvc-rg"
-}
+# data "azurerm_private_dns_zone" "core-infra-intsvc" {
+#   provider            = azurerm.private-endpoint-dns
+#   name                = "privatelink.blob.core.windows.net"
+#   resource_group_name = "core-infra-intsvc-rg"
+# }
 
 # data "azurerm_log_analytics_workspace" "core" {
 #   name                = "vh-infra-core-${var.environment}"
